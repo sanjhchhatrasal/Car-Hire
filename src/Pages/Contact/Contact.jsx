e8c6aca340cc30270f82e87c9c6d0f1864d7a2d6
Sanjh Chhatrasal <sanjhchhatrasal@gmail.com>
2024-05-14 22:04:17 +0530

    Contact

--- src/Pages/Contact/Contact.jsx
+++ src/Pages/Contact/Contact.jsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 function Contact() {
   return (
-    <div className='h-screen w-full bg-sky-700'>Contact</div>
+    <div className='h-screen w-full bg-sky-700'>
+      <h1>This is contact Page</h1>
+    </div>
   )
 }
 
