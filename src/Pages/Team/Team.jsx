d5f151827cee9a5e2054d06659e2097924792237
Sanjh Chhatrasal <sanjhchhatrasal@gmail.com>
2024-05-14 22:03:13 +0530

    team

--- src/Pages/Team/Team.jsx
+++ src/Pages/Team/Team.jsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 function Team() {
   return (
-    <div className='w-full h-screen bg-blue-200'>Team</div>
+    <div className='w-full h-screen bg-blue-200'>
+      <h1>This is Teams Page</h1>
+    </div>
   )
 }
 
