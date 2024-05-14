e29321073123b9c7c21ba73d63e566da6125f0a2
Sanjh Chhatrasal <sanjhchhatrasal@gmail.com>
2024-05-14 22:02:42 +0530

    model

--- src/Pages/Models/Models.jsx
+++ src/Pages/Models/Models.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Models() {
   return (
-    <div className='w-full h-screen bg-cyan-700 color-black'>
+    <div className='w-full h-screen bg-cyan-700 color-black flex items-center justify-center'>
       <h1 className='text-2xl font-semibold'>This is the model page</h1>
     </div>
   )
