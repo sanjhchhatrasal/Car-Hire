28af3ecdefb51e5084d189964f19d8ee1055e6ff
Sanjh Chhatrasal <sanjhchhatrasal@gmail.com>
2024-05-15 19:56:47 +0530

    About page

--- src/Pages/About/About.jsx
+++ src/Pages/About/About.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function About() {
   return (
     <div className='h-screen w-full bg-red-200 flex items-center justify-center'>
-      <h1>About page</h1>
+      <h1 className='text-2xl font-semibold'>About page</h1>
     </div>
   )
 }
