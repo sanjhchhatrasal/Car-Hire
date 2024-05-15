bd2e48ff84a3f6a09df8eb41ac2a7d28cbe72189
Sanjh Chhatrasal <sanjhchhatrasal@gmail.com>
2024-05-15 19:57:15 +0530

    Teams page

--- src/Pages/Team/Team.jsx
+++ src/Pages/Team/Team.jsx
@@ -4,6 +4,7 @@ function Team() {
   return (
     <div className='w-full h-screen bg-blue-200 flex items-center justify-center'>
       <h1 className='text-2xl font-semibold'>This is Teams Page</h1>
+      <h2>Hello from teams page</h2>
     </div>
   )
 }
