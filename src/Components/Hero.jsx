ec2f8dab7ed6d2de147b1304a29073710c720426
Sanjh Chhatrasal <sanjhchhatrasal@gmail.com>
2024-05-15 19:58:58 +0530

    hero

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 function Hero() {
   return (
-    <div className='h-screen w-full bg-sky-200'>
-        <h1>This is Hero page</h1>
+    <div className='h-screen w-full bg-sky-200 flex items-center justify-center'>
+        <h1 className='text-2xl font-semibold'>This is Hero page</h1>
     </div>
   )
 }
