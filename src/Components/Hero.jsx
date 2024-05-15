07289f16d04686b0b657297aa7c328b6a9b149c8
Sanjh Chhatrasal <sanjhchhatrasal@gmail.com>
2024-05-15 19:58:35 +0530

    hero

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 function Hero() {
   return (
-    <div className='h-screen w-full bg-sky-200'>Hero</div>
+    <div className='h-screen w-full bg-sky-200'>
+        <h1>This is Hero page</h1>
+    </div>
   )
 }
 
