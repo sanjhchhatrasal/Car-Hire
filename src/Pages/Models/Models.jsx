6c5984a5a14966f23406a5ce33775c26039b393a
Sanjh Chhatrasal <sanjhchhatrasal@gmail.com>
2024-05-16 12:40:46 +0530

    Models Page

--- src/Pages/Models/Models.jsx
+++ src/Pages/Models/Models.jsx
@@ -5,6 +5,8 @@ function Models() {
     <div className='w-full h-screen bg-cyan-700 color-black flex items-center justify-center'>
       <h1 className='text-2xl font-semibold'>This is the model page</h1>
       <h2 className='text-xl font-semibold'>Hello from models page</h2>
+      <p className='text-xl text-zinc-300'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis vitae, iure amet soluta velit dolorum facilis, aspernatur laudantium dolor officia tempora sapiente illo deserunt ab. Perferendis incidunt amet quae nam.</p>
+
     </div>
   )
 }
