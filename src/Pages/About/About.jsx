816c2eb3910577528c386618355441d0decb973f
Sanjh Chhatrasal <sanjhchhatrasal@gmail.com>
2024-05-17 19:16:30 +0530

    about page

--- src/Pages/About/About.jsx
+++ src/Pages/About/About.jsx
@@ -5,7 +5,6 @@ function About() {
     <div className='h-screen w-full bg-red-200 flex items-center justify-center'>
       <h1 className='text-2xl font-semibold'>About page</h1>
       <h2 className='text-xl font-semibold'>Hello from about page</h2>
-      <p className='text-xl text-zinc-300'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis vitae, iure amet soluta velit dolorum facilis, aspernatur laudantium dolor officia tempora sapiente illo deserunt ab. Perferendis incidunt amet quae nam.</p>
     </div>
   )
 }
