8d4f9a22cc9d7327b3b1d7c520d21b88819f4d25
Sanjh Chhatrasal <sanjhchhatrasal@gmail.com>
2024-05-18 22:03:18 +0530

    Hero.jsx

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -4,6 +4,8 @@ function Hero() {
   return (
     <div className='h-screen w-full bg-sky-200 flex items-center justify-center'>
         <h1 className='text-2xl font-semibold'>This is Hero page</h1>
+        <h2>hello from Hero page</h2>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum itaque debitis exercitationem, voluptatem omnis quam ipsum harum! Quas, libero cupiditate?</p>
     </div>
   )
 }
