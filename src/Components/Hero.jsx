e39ef20e87def5ce117c4505ffbd2675bd37c778
Sanjh Chhatrasal <sanjhchhatrasal@gmail.com>
2024-05-21 22:40:25 +0530

    Hero file

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -5,7 +5,7 @@ function Hero() {
     <div className='h-screen w-full bg-sky-200 flex items-center justify-center'>
         <h1 className='text-2xl font-semibold'>This is Hero page</h1>
         <h2 className='text-xl font-semibold'>hello from Hero page</h2>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum itaque debitis exercitationem, voluptatem omnis quam ipsum harum! Quas, libero cupiditate?</p>
+        <p className='text-xl font-semibold'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum itaque debitis exercitationem, voluptatem omnis quam ipsum harum! Quas, libero cupiditate?</p>
     </div>
   )
 }
