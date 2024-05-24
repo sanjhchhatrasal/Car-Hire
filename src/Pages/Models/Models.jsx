e845c82b51b3ce252504d3b95c0cacf4528eddde
Sanjh Chhatrasal <sanjhchhatrasal@gmail.com>
2024-05-24 20:26:56 +0530

    model.jsx

--- src/Pages/Models/Models.jsx
+++ src/Pages/Models/Models.jsx
@@ -5,7 +5,6 @@ function Models() {
     <div className='w-full h-screen bg-cyan-700 color-black flex items-center justify-center'>
       <h1 className='text-2xl font-semibold'>This is the model page</h1>
       <h2 className='text-xl font-semibold'>hello from Model page</h2>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo, autem! Eligendi adipisci ab voluptas consequatur repudiandae neque dolor tempore repellat corrupti aut, recusandae tenetur alias. Esse illum quae qui quos.</p>
       <p className='text-xl font-semibold'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium fuga, illum adipisci ex nam cupiditate error nobis voluptates esse natus!</p>
 
     </div>
