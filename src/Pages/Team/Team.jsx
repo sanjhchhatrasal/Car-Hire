0c2f0594ecce4414b05e23208ceb018ed5f371bb
Sanjh Chhatrasal <sanjhchhatrasal@gmail.com>
2024-05-24 20:24:09 +0530

    Team.jsx

--- src/Pages/Team/Team.jsx
+++ src/Pages/Team/Team.jsx
@@ -5,6 +5,7 @@ function Team() {
     <div className='w-full h-screen bg-blue-200 flex items-center justify-center'>
       <h1 className='text-2xl font-semibold'>This is Teams Page</h1>
       <h2 className='text-xl font-semibold'>hello from Team page</h2>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae aspernatur assumenda neque? Quidem assumenda culpa, nostrum, accusamus similique labore velit deleniti officia eum soluta magni quos molestiae odio expedita at.</p>
       <p className='text-xl font-semibold'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat, laudantium.</p>
     </div>
   )
