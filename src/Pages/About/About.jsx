3d3e8d305d179da9120fbf286f2f2f6007a16ba2
Sanjh Chhatrasal <sanjhchhatrasal@gmail.com>
2024-05-28 20:35:05 +0530

    About

--- src/Pages/About/About.jsx
+++ src/Pages/About/About.jsx
@@ -4,7 +4,6 @@ function About() {
   return (
     <div className='h-screen w-full bg-red-200 flex items-center justify-center'>
       <h1 className='text-2xl font-semibold'>About page</h1>
-      <h2 className='text-xl font-semibold'> <h2>hello from About page</h2></h2>
     </div>
   )
 }
