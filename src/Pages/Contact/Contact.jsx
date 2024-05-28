f5ece787aed491b82c778ac8b1e35e365f5593cb
Sanjh Chhatrasal <sanjhchhatrasal@gmail.com>
2024-05-28 20:34:49 +0530

    Contact

--- src/Pages/Contact/Contact.jsx
+++ src/Pages/Contact/Contact.jsx
@@ -4,7 +4,6 @@ function Contact() {
   return (
     <div className='h-screen w-full bg-sky-700 flex items-center justify-center'>
       <h1 className='text-2xl font-semibold'>This is contact Page</h1>
-      <h2 className='text-xl font-semibold'>hello from Contact page</h2>
     </div>
   )
 }
