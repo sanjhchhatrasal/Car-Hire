0929c367a4663bf174d913ce139e783779a1c0b1
Sanjh Chhatrasal <sanjhchhatrasal@gmail.com>
2024-06-02 12:05:38 +0530

    Conatct

--- src/Pages/Contact/Contact.jsx
+++ src/Pages/Contact/Contact.jsx
@@ -5,6 +5,7 @@ function Contact() {
     <div className='h-screen w-full bg-sky-700 flex items-center justify-center'>
       <h1 className='text-2xl font-semibold'>This is contact Page</h1>
       <h2 className='text-xl font-semibold bg-sky-300'>Sub Heading</h2>
+      <h2 className='text-xl font-semibold bg-sky-300'>Sub Heading</h2>
     </div>
   )
 }
