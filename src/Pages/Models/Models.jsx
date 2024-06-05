b131f97c46899f4609f142f0ceb01a0f7e54b871
Sanjh Chhatrasal <sanjhchhatrasal@gmail.com>
2024-06-05 13:49:09 +0530

    Model.jsx

--- src/Pages/Models/Models.jsx
+++ src/Pages/Models/Models.jsx
@@ -5,7 +5,6 @@ function Models() {
     <div className='w-full h-screen bg-cyan-700 color-black flex items-center justify-center'>
       <h1 className='text-2xl font-semibold'>This is the model page</h1>
       <h2 className='text-xl font-semibold bg-sky-300'>Sub Heading</h2>
-      <h2 className='text-xl font-semibold bg-sky-300'>Sub Heading</h2>
 
     </div>
   )
