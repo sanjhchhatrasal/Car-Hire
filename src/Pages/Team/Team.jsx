6d36245a1f049d8a9fd33c308dda5b8d81c2d7fc
Sanjh Chhatrasal <sanjhchhatrasal@gmail.com>
2024-06-05 13:49:46 +0530

    Team.jsx

--- src/Pages/Team/Team.jsx
+++ src/Pages/Team/Team.jsx
@@ -5,7 +5,6 @@ function Team() {
     <div className='w-full h-screen bg-blue-200 flex items-center justify-center'>
       <h1 className='text-2xl font-semibold'>This is Teams Page</h1>
       <h2 className='text-xl font-semibold bg-sky-300'>Sub Heading</h2>
-      <h2 className='text-xl font-semibold bg-sky-300'>Sub Heading</h2>
     </div>
   )
 }
