35d989bc7df088eff61f75baf92fa3642d0da644
Sanjh Chhatrasal <sanjhchhatrasal@gmail.com>
2024-06-16 11:24:32 +0530

    Hero

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -5,6 +5,7 @@ function Hero() {
     <div className='h-screen w-full bg-sky-200 flex items-center justify-center'>
         <h1 className='text-2xl font-semibold'>This is Hero page</h1>
         <h2 className='font-semibold text-xl'>hero</h2>
+        
     </div>
   )
 }
